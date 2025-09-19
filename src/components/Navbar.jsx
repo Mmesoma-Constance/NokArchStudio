acd0539be52ac27d87e0a74dc761c43fa957040e
Mmesoma-Constance <mmesomanwokem@gmail.com>
2025-09-19 03:07:48 +0100

    added font-family in mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,7 +106,7 @@ const Navigation = () => {
         </div>
 
         {/* Mobile Menu Panel */}
-        <div className={`md:hidden transition-all duration-300 ease-out ${
+        <div className={`md:hidden font-montserrat transition-all duration-300 ease-out ${
           isMobileMenuOpen 
             ? 'max-h-96 opacity-100' 
             : 'max-h-0 opacity-0 overflow-hidden'
